058041e9dd4525af96e06177029626cab0f1e3b8
Matheus Veiga Lima <mveigadelima@gmail.com>
2025-02-13 21:36:02 -0300

    melhorando o código

--- api/src/server.js
+++ api/src/server.js
@@ -2,7 +2,8 @@ import app from "./app.js";
 import { connectDB } from "./database.js";
 
 const PORT = process.env.PORT || 4000;
+const HOST = "0.0.0.0"; 
 
 connectDB().then(() => {
-  app.listen(PORT, () => console.log(`✅ Server  is running on port ${PORT}`));
+  app.listen(PORT, HOST, () => console.log(`✅ Server  is running on port ${PORT}`));
 });
--- web/src/pages/HeroesList.js
+++ web/src/pages/HeroesList.js
@@ -11,6 +11,7 @@ const HeroesList = () => {
     const [error, setError] = useState(null);
     const [loading, setLoading] = useState(false);
 
+
     useEffect(() => {
         setLoading(true);
         getHeroes()
